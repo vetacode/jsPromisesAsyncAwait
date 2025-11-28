5d6a734b8a66f3e56f052ffea15034d7ecd18c45
vetacode <vetacode@gmail.com>
2025-11-28 15:35:52 +0700

    adding solution to callback hell

--- callback.js
+++ callback.js
@@ -9,3 +9,55 @@ loadScript('/my/script.js', function (script) {
     console.log(`Cool, the second script is loaded`);
   });
 });
+
+//PYRAMID OF DOOM/ CALLBACK HELL
+loadScript('1.js', function (error, script) {
+  if (error) {
+    handleError(error);
+  } else {
+    // ...
+    loadScript('2.js', function (error, script) {
+      if (error) {
+        handleError(error);
+      } else {
+        // ...
+        loadScript('3.js', function (error, script) {
+          if (error) {
+            handleError(error);
+          } else {
+            // ...continue after all scripts are loaded (*)
+          }
+        });
+      }
+    });
+  }
+});
+
+//SOLUTION
+loadScript('1.js', step1);
+
+function step1(error, script) {
+  if (error) {
+    handleError(error);
+  } else {
+    // ...
+    loadScript('2.js', step2);
+  }
+}
+
+function step2(error, script) {
+  if (error) {
+    handleError(error);
+  } else {
+    // ...
+    loadScript('3.js', step3);
+  }
+}
+
+function step3(error, script) {
+  if (error) {
+    handleError(error);
+  } else {
+    // ...continue after all scripts are loaded (*)
+  }
+}
