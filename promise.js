55f7bfd6f4f7f61e39f01d252c842b043a2b4fbd
vetacode <vetacode@gmail.com>
2025-11-28 17:28:39 +0700

    task done

--- promise.js
+++ promise.js
@@ -159,3 +159,23 @@ promise.then(alert);
 
   promise.then(console.log);
 }
+
+/**TASK 2
+ * Delay with a promise
+The built-in function setTimeout uses callbacks. Create a promise-based alternative.
+
+The function delay(ms) should return a promise. That promise should resolve after ms milliseconds, so that we can add .then to it, like this:
+
+function delay(ms) {
+  // your code
+}
+
+delay(3000).then(() => alert('runs after 3 seconds'));
+ */
+
+function delay(ms) {
+  // your code
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+delay(3000).then(() => console.log('runs after 3 seconds'));
