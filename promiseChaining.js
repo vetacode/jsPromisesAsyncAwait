eb96743373d9bc44ea0bc8049ab2715f42e3be0b
vetacode <vetacode@gmail.com>
2025-11-30 12:44:44 +0700

    adding making reusable async  function

--- promiseChaining.js
+++ promiseChaining.js
@@ -109,3 +109,75 @@ fetch('/article/promise-chaining/user.json')
     // ...and here's the content of the remote file
     console.log(text); // {"name": "iliakan", "isAdmin": true}
   });
+
+//Fetch to github example
+// Make a request for user.json
+fetch('/article/promise-chaining/user.json')
+  // Load it as json
+  .then((response) => response.json())
+  // Make a request to GitHub
+  .then((user) => fetch(`https://api.github.com/users/${user.name}`))
+  // Load the response as json
+  .then((response) => response.json())
+  // Show the avatar image (githubUser.avatar_url) for 3 seconds (maybe animate it)
+  .then((githubUser) => {
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = 'promise-avatar-example';
+    document.body.append(img);
+
+    setTimeout(() => img.remove(), 3000); // (*)
+  });
+
+//Should be like this:
+fetch('/article/promise-chaining/user.json')
+  .then((response) => response.json())
+  .then((user) => fetch(`https://api.github.com/users/${user.name}`))
+  .then((response) => response.json())
+  .then(
+    (githubUser) =>
+      new Promise(function (resolve, reject) {
+        // (*)
+        let img = document.createElement('img');
+        img.src = githubUser.avatar_url;
+        img.className = 'promise-avatar-example';
+        document.body.append(img);
+
+        setTimeout(() => {
+          img.remove();
+          resolve(githubUser); // (**)
+        }, 3000);
+      })
+  )
+  // triggers after 3 seconds
+  .then((githubUser) => alert(`Finished showing ${githubUser.name}`));
+
+//Make the function reusable
+function loadJson(url) {
+  return fetch(url).then((response) => response.json());
+}
+
+function loadGithubUser(name) {
+  return loadJson(`https://api.github.com/users/${name}`);
+}
+
+function showAvatar(githubUser) {
+  return new Promise(function (resolve, reject) {
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = 'promise-avatar-example';
+    document.body.append(img);
+
+    setTimeout(() => {
+      img.remove();
+      resolve(githubUser);
+    }, 3000);
+  });
+}
+
+// Use them:
+loadJson('/article/promise-chaining/user.json')
+  .then((user) => loadGithubUser(user.name))
+  .then(showAvatar)
+  .then((githubUser) => alert(`Finished showing ${githubUser.name}`));
+// ...
