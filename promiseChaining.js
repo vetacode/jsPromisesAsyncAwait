b70aaf1c7a9e647073634df822619fcd0f3aeada
vetacode <vetacode@gmail.com>
2025-11-30 09:55:19 +0700

    adding returning promises

--- promiseChaining.js
+++ promiseChaining.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//.THEN chaining
+//.THEN handler chaining
 //every call to a .then returns a new promise
 
 new Promise(function (resolve, reject) {
@@ -19,3 +19,26 @@ new Promise(function (resolve, reject) {
     console.log(result); //4
     return result * 2;
   });
+
+//Returning promises: allows to build chains of asynchronous actions.
+
+{
+  new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(1), 1000);
+  })
+    .then(function (result) {
+      console.log(result); // 1
+      return new Promise(function (resolve, reject) {
+        setTimeout(() => resolve(result * 2), 2000);
+      });
+    })
+    .then(function (result) {
+      console.log(result); //2
+      return new Promise(function (resolve, reject) {
+        setTimeout(() => resolve(result * 2), 3000);
+      });
+    })
+    .then(function (result) {
+      console.log(result); //4
+    });
+}
