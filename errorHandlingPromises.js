dbc0ab878230338e0fe90f4e671c2c706105ad00
vetacode <vetacode@gmail.com>
2025-11-30 22:05:31 +0700

    adding unhandled rejection

--- errorHandlingPromises.js
+++ errorHandlingPromises.js
@@ -54,3 +54,16 @@ new Promise((resolve, reject) => {
 }
 
 //Unhandled rejections
+//In the browser we can catch such errors using the event unhandledrejection
+
+/**
+ * window.addEventListener('unhandledrejection', function(event) {
+  // the event object has two special properties:
+  alert(event.promise); // [object Promise] - the promise that generated the error
+  alert(event.reason); // Error: Whoops! - the unhandled error object
+});
+
+new Promise(function() {
+  throw new Error("Whoops!");
+}); // no catch to handle the error
+ */
