26ec4a2c4c90563d2d903f1272808fb7cbdd9656
vetacode <vetacode@gmail.com>
2025-11-30 22:01:09 +0700

    adding Rethrowing error

--- errorHandlingPromises.js
+++ errorHandlingPromises.js
@@ -9,6 +9,48 @@ fetch('https://nowebsitehere.com')
 console.log(1 == '1');
 
 //Implicit try…catch
+//.catch not only catches explicit rejections, but also accidental errors in the handlers
 new Promise((resolve, reject) => {
   reject(new Error('WTF!!!'));
 }).catch(console.error);
+
+//Rethrowing
+{
+  // the execution: catch -> then
+  new Promise((resolve, reject) => {
+    throw new Error('Whoops!');
+  })
+    .catch(function (error) {
+      console.log('The error is handled, continue normally');
+    })
+    .then(() => console.log('Next successful handler runs'));
+}
+
+{
+  // the execution: catch -> catch
+  new Promise((resolve, reject) => {
+    throw new Error('Whoops!');
+  })
+    .catch(function (error) {
+      // (*)
+
+      if (error instanceof URIError) {
+        // handle it
+      } else {
+        console.log("Can't handle such error");
+
+        throw error; // throwing this or another error jumps to the next catch
+      }
+    })
+    .then(function () {
+      /* doesn't run here */
+    })
+    .catch((error) => {
+      // (**)
+
+      console.log(`The unknown error has occurred: ${error}`);
+      // don't return anything => execution goes the normal way
+    });
+}
+
+//Unhandled rejections
